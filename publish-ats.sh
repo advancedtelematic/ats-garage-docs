092f567dd1425bf3d0dc9247b9d35fda59b05cc6
Alex Humphreys <alex.humphreys@here.com>
2019-01-02 18:05:04 +0100

    Deploy docs to s3 bucket docs.ota.here.com
    Signed-off-by: Alex Humphreys <alex.humphreys@here.com>

--- publish-ats.sh
+++ publish-ats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-[ -z "$1" ] && echo "Usage: $0 <staging|prod>" && exit 1
+[ -z "$1" ] && echo "Usage: $0 <staging|prod|here>" && exit 1
 
 env_file=s3_credentials.env
 
@@ -21,7 +21,11 @@ elif [ "$1" == "staging" ]; then
   docker run --rm -v "$(pwd)":/site -u "$(id -u)":"$(id -g)" advancedtelematic/jekyll-asciidoc jekyll b --config _conf-here.yml,_secrets.yml --destination ./_site-ats
   docker run --rm -v "$(pwd)":/project -u "$(id -u)":"$(id -g)" --env-file=$env_file mesosphere/aws-cli s3 sync --delete --exclude *.mp4 _site-ats/ s3://docs-staging.atsgarage.com
   echo "Docsite pushed to http://docs-staging.atsgarage.com.s3-website.eu-central-1.amazonaws.com/"
+elif [ "$1" == "here" ]; then
+  docker run --rm -v "$(pwd)":/site -u "$(id -u)":"$(id -g)" advancedtelematic/jekyll-asciidoc jekyll b --config _conf-ats.yml,_secrets.yml --destination ./_site-ats
+  docker run --rm -v "$(pwd)":/project -u "$(id -u)":"$(id -g)" --env-file=$env_file mesosphere/aws-cli s3 sync --delete --exclude *.mp4 _site-ats/ s3://docs.ota.here.com
+  echo "Docsite pushed to https://docs.ota.here.com/"
 else
-  echo "Usage: $0 <staging|prod>" && exit 1
+  echo "Usage: $0 <staging|prod|here>" && exit 1
 fi
 rm -rf _site-ats
